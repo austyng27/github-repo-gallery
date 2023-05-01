c7e5dc4e65ab2ec6e103e52a96854bd0fc80e1f9
Austyn G <107218581+austyng27@users.noreply.github.com>
2023-05-01 15:19:36 -0500

    Added repo display

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 //Profile Info
 const overview = document.querySelector(".overview");
-
 //Github username
 const username = "austyng27";
+//Repo Display List Element
+const repoList = document.querySelector(".repo-list");
 
 //Github API to retrieve user info
 const getUserInfo = async function(){
     const response = await fetch(`https://api.github.com/users/${username}`);
     const data = await response.json();
-    console.log(data);
     userInfoDisplay(data);
 };
 getUserInfo();
@@ -27,4 +27,22 @@ const userInfoDisplay = function(data){
     <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
   </div>`;
   overview.append(div);
+  getRepos();
+};
+
+//API to retrieve public repos
+const getRepos = async function(){
+    const repoResponse = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await repoResponse.json();
+    displayRepos(repoData);
+};
+
+//Display info about each repo
+const displayRepos = function(repos){
+    for(const repo of repos){
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
 };
